2ef21ac847c6b027e52d39da3a263613487b3187
howmanysmall <9477871+1@iechs.org>
2018-03-30 09:47:42 -0600

    Create TableLibrary.lua

---
+++ TableLibrary.lua
@@ -0,0 +1,35 @@
+local TableLibrary = { }
+
+function TableLibrary:BinSearch(Data, Query)
+	assert(type(Data) == "table", "Data is not a table.")
+	local Left = 1
+	local Right = #Data
+	while Left <= Right do
+		local Middle = math.floor(0.5 * (Left + Right))
+		if Data[Middle] == Query then
+			return Middle
+		elseif Data[Middle] > Query then
+			Right = Middle - 1
+		else
+			Left = Middle + 1
+		end
+	end
+end
+
+function TableLibrary:Search(Data, Query, DeepSearch)
+	assert(type(Data) == "table", "Data is not a table.")
+	DeepSearch = DeepSearch or false
+	for Index = 1, #Data do
+		local Value = Data[Index]
+		if Value == Query then
+			return true, Index, Value
+		elseif DeepSearch and type(Value) == "table" then
+			return TableLibrary:Search(Data, Query, true)
+		end
+	end
+end
+
+function TableLibrary:Flatten(Data)
+end
+
+return TableLibrary
